70a1e8b006eb27fa45d75425b4188eba65dfbb40
Lee Watson <rev@revthefox.co.uk>
2021-06-20 23:36:34 +0100

    Add data directory.

--- rehome/__init__.py
+++ rehome/__init__.py
@@ -4,9 +4,9 @@ from flask import Flask
 
 BASE_DIR = Path(__file__).parent.absolute()
 CONFIG_DIR = BASE_DIR.parent / "config"
+DATA_DIR = BASE_DIR.parent / "data"
 STATIC_DIR = BASE_DIR / "static"
 ASSETS_DIR = BASE_DIR / "assets"
-STORAGE_DIR = BASE_DIR / "storage"
 TEMPLATE_DIR = BASE_DIR / "templates"
 
 
