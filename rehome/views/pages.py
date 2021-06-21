f000fee1a3ea52f87c41d8af5c0bf7f7bfe4ae2f
Lee Watson <rev@revthefox.co.uk>
2021-06-21 21:13:07 +0100

    use get() instead of route()

--- rehome/views/pages.py
+++ rehome/views/pages.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 blueprint = Blueprint("pages", __name__)
 
 
-@blueprint.route("/")
+@blueprint.get("/")
 def index():
     return render_template("pages/index.html")
