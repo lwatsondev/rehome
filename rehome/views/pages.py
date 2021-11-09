d38aed08ab1340cc51b62789ca29ed98014dc0a9
Lee Watson <rev@revthefox.co.uk>
2021-11-09 15:22:24 +0000

    Add per-domain profile links

--- rehome/views/pages.py
+++ rehome/views/pages.py
@@ -1,12 +1,20 @@
+from urllib.parse import urlparse
+
 from flask import Blueprint
 from flask import current_app as app
-from flask import render_template
+from flask import render_template, request
 
 blueprint = Blueprint("pages", __name__)
 
 
 @blueprint.get("/")
 def index():
+    http_host_key = urlparse(request.base_url).hostname.replace(".", "_")
+
     return render_template(
-        "pages/index.html", profile_links=app.config.get("rehome.profile.external")
+        "pages/index.html",
+        profile_links=app.config.get(
+            f"rehome.profile.external.{http_host_key}",
+            app.config.get("rehome.profile.external.default"),
+        ),
     )
