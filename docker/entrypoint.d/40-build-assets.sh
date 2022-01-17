4a5457f2b9d93d6772f305f8281104988889c625
Lee Watson <me@lwatson.dev>
2022-01-17 13:48:53 +0000

    Remove rehome/static as it's only used for images

--- docker/entrypoint.d/40-build-assets.sh
+++ docker/entrypoint.d/40-build-assets.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Updating assets"
-runuser -p -u rehome -- rsync -rau /app/rehome/static/img/ "$FLASK_STATIC_DIR/img/"
+runuser -p -u rehome -- rsync -rau /app/rehome/assets/img/ "$FLASK_STATIC_DIR/img/"
 runuser -p -u rehome -- flask assets clean || true
 runuser -p -u rehome -- flask assets build
