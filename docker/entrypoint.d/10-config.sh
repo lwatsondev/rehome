70538ddab5c0ecb61a78be611d32f30f006f3274
Lee Watson <me@lwatson.dev>
2022-01-18 23:00:00 +0000

    Fix entrypoint.d `cp` commands

--- docker/entrypoint.d/10-config.sh
+++ docker/entrypoint.d/10-config.sh
@@ -2,5 +2,5 @@
 
 if [ ! -f "$SETTINGS_FILE_FOR_DYNACONF" ]; then
     echo "Copying default settings.yml"
-    cp -rau /app/rehome/resources/config/settings.yml "$SETTINGS_FILE_FOR_DYNACONF"
+    cp -au /app/rehome/resources/config/settings.yml "$SETTINGS_FILE_FOR_DYNACONF"
 fi
