3930f8336e85e2a9bb05bd7a7669d86b282a74e5
Lee Watson <me@lwatson.dev>
2022-01-19 00:19:41 +0000

    Use rsync to copy files. Might as well. It's there.

--- docker/entrypoint.d/10-config.sh
+++ docker/entrypoint.d/10-config.sh
@@ -2,5 +2,5 @@
 
 if [ ! -f "$SETTINGS_FILE_FOR_DYNACONF" ]; then
     echo "Copying default settings.yml"
-    cp -au /app/rehome/resources/config/settings.yml "$SETTINGS_FILE_FOR_DYNACONF"
+    rsync -auq /app/rehome/resources/config/settings.yml "$SETTINGS_FILE_FOR_DYNACONF"
 fi
--- docker/entrypoint.d/40-build-assets.sh
+++ docker/entrypoint.d/40-build-assets.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-runuser -u "$APP_USER" -- rsync -rau /app/rehome/assets/img/ "$FLASK_STATIC_DIR/img/"
+runuser -u "$APP_USER" -- rsync -rauq /app/rehome/assets/img/ "$FLASK_STATIC_DIR/img/"
 runuser -u "$APP_USER" -- flask assets clean || true
 runuser -u "$APP_USER" -- flask assets build
