d929e7819e16b3b04130771f703620c3ae39ce23
Lee Watson <me@lwatson.dev>
2024-02-22 20:55:43 +0000

    Remove redundant root_path logic

--- rehome/config.py
+++ rehome/config.py
@@ -1,11 +1,8 @@
-import os
-
 from dynaconf import Dynaconf
 
 dynaconf = Dynaconf(
     environments=False,
     envvar_prefix="CFG",
-    root_path=os.getenv("ROOT_PATH_FOR_DYNACONF", None),
     settings_files=[
         "*.toml",
         "*.yml",
