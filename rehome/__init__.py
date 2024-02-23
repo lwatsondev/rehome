130fa3ca9a001af4921fd486322fd94af9f26464
Lee Watson <me@lwatson.dev>
2024-02-23 00:28:35 +0000

    Use ProxyFix correctly

--- rehome/__init__.py
+++ rehome/__init__.py
@@ -31,7 +31,7 @@ def create_app() -> Flask:
         register_assets(app)
 
     if not app.debug and not app.testing:
-        ProxyFix(app)
+        app.wsgi_app = ProxyFix(app.wsgi_app)
 
     return app
 
