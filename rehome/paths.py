bfc8f2906dd40862bb49cc5c6bc58ac496a1f16f
Lee Watson <me@lwatson.dev>
2025-04-11 22:39:14 +0100

    Prefer `import ... as ...` for paths config.

--- rehome/paths.py
+++ rehome/paths.py
@@ -1,14 +1,11 @@
 from pathlib import Path
 
-from rehome import config
+# Needs to be loaded outside the flask extension as these are needed before we have an app context.
+from rehome.config import dynaconf as _config
 
 BASE = Path(__file__).parent.absolute()
-
-# Needs to be loaded outside the flask extension as these are needed before we have an app context.
-__config = config.dynaconf
-
-TEMPLATES = Path(__config.get("paths.templates", BASE / "templates"))
-RESOURCES = Path(__config.get("paths.resources", BASE / "resources"))
-DATA = Path(__config.get("paths.data", BASE.parent / "data"))
-STATIC = Path(__config.get("paths.static", BASE / "static"))
-UPLOADS = Path(__config.get("paths.uploads", DATA / "uploads"))
+TEMPLATES = Path(_config.get("paths.templates", BASE / "templates"))
+RESOURCES = Path(_config.get("paths.resources", BASE / "resources"))
+DATA = Path(_config.get("paths.data", BASE.parent / "data"))
+STATIC = Path(_config.get("paths.static", BASE / "static"))
+UPLOADS = Path(_config.get("paths.uploads", DATA / "uploads"))
