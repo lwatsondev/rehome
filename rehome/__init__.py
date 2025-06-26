a9e91efd5971b1ea13f9888ec67e7e67e64713d8
Lee Watson <me@lwatson.dev>
2025-06-26 13:22:36 +0100

    Fix broken DB session caused by early view import

--- rehome/__init__.py
+++ rehome/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask, url_for
 from libgravatar import Gravatar
 from werkzeug.middleware.proxy_fix import ProxyFix
 
-from rehome import meta, paths, views
+from rehome import meta, paths
 from rehome.extensions import db, debugbar, dynaconf
 
 
@@ -36,6 +36,8 @@ def create_app() -> Flask:
 
 
 def register_blueprints(app: Flask):
+    from rehome import views  # noqa: PLC0415
+
     views.register_blueprints(app)
 
 
