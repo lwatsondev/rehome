781af8e0890d978931dacd18c03a9fd19183ed63
Lee Watson <me@lwatson.dev>
2025-12-22 01:35:53 +0000

    Update github urls

--- rehome/meta.py
+++ rehome/meta.py
@@ -3,7 +3,7 @@ import os
 FULL_VERSION: str = "[version unknown]"
 VERSION: str | None = os.getenv("META_VERSION")
 COMMIT: str | None = os.getenv("META_COMMIT")
-SOURCE: str = os.getenv("META_SOURCE") or "https://github.com/TheReverend403/rehome"
+SOURCE: str = os.getenv("META_SOURCE") or "https://github.com/lwatsondev/rehome"
 
 if VERSION:
     FULL_VERSION = f"{VERSION}"
