0d5bd9f6f393c99e1e83acdcec30d57716175956
Lee Watson <me@lwatson.dev>
2026-05-02 15:36:19 +0100

    Uppercase private constants in config.py

--- rehome/config.py
+++ rehome/config.py
@@ -3,12 +3,12 @@ from pathlib import Path
 
 from dynaconf import Dynaconf
 
-_base_dir = Path(__file__).parent
-_config_dir = os.getenv("ROOT_PATH_FOR_DYNACONF", _base_dir / "resources" / "config")
+_BASE_DIR = Path(__file__).parent
+_CONFIG_DIR = os.getenv("ROOT_PATH_FOR_DYNACONF", _BASE_DIR / "resources" / "config")
 
 dynaconf = Dynaconf(
     environments=False,
     envvar_prefix="CFG",
-    root_path=_config_dir,
-    settings_files=[str(_base_dir / "resources" / "config" / "default.toml"), "*.toml"],
+    root_path=_CONFIG_DIR,
+    settings_files=[str(_BASE_DIR / "resources" / "config" / "default.toml"), "*.toml"],
 )
