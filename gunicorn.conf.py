4758ff3aa2b8018b350fb391719f8a1e944981e6
Lee Watson <me@lwatson.dev>
2026-05-04 13:51:12 +0100

    Switch gunicorn worker class from gevent to gthread

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,8 +3,9 @@ import os
 
 bind = os.getenv("GUNICORN_BIND", "127.0.0.1:5000")
 wsgi_app = os.getenv("GUNICORN_APP", f"{os.getenv('FLASK_APP')}:create_app()")
-worker_class = os.getenv("GUNICORN_WORKER_CLASS", "gevent")
-workers = os.getenv("GUNICORN_WORKERS", multiprocessing.cpu_count())
+worker_class = os.getenv("GUNICORN_WORKER_CLASS", "gthread")
+threads = os.getenv("GUNICORN_THREADS", multiprocessing.cpu_count() * 2)
+workers = os.getenv("GUNICORN_WORKERS", multiprocessing.cpu_count() * 2)
 timeout = os.getenv("GUNICORN_TIMEOUT", "120")
 max_requests = os.getenv("GUNICORN_MAX_REQUESTS", "1000")
 max_requests_jitter = os.getenv("GUNICORN_MAX_REQUESTS_JITTER", "100")
