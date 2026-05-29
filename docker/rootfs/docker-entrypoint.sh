9cd087d2e4fc30f17655c26dee2dc9e7b6c6a982
Lee Watson <me@lwatson.dev>
2026-05-30 00:04:35 +0100

    Watch TOML config files in dev server reloader

--- docker/rootfs/docker-entrypoint.sh
+++ docker/rootfs/docker-entrypoint.sh
@@ -4,7 +4,8 @@ set -eu
 flask db migrate
 
 if [ "${FLASK_DEBUG:-0}" = 1 ]; then
-    flask run --host=0.0.0.0
+    flask run --host=0.0.0.0 \
+        --extra-files "/app/rehome/resources/config/default.toml:${ROOT_PATH_FOR_DYNACONF:-/config}"
 fi
 
 exec gunicorn --config /app/gunicorn.conf.py
